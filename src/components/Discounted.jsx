fc58c4665973271e73d6207f16f0fca1c3005527
coding4freedom <chawkins2010@hotmail.com>
2024-09-11 22:40:06 -0500

    first commit, Nav, Landing, Highlights, Featured components

---
+++ src/components/Discounted.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Discounted() {
+  return (
+    <section id="recent">
+        <div className="container">
+            <div className="row">
+                <div className="section__title">
+                    Discount <span className="purple">Books</span>
+                </div>
+            </div>
+            <div className="books">
+                
+            </div>
+        </div>
+    </section>
+  )
+}
