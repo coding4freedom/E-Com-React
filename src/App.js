a0f849d853823ad40efacaf3cc6405def865964d
coding4freedom <chawkins2010@hotmail.com>
2024-09-12 15:11:55 -0500

    Explore, Footer components added, updated Discounted star logic for books

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import Landing from "./components/Landing";
 import Nav from "./components/Nav";
 import Highlights from "./components/Highlights";
 import Featured from "./components/Featured";
+import Discounted from "./components/Discounted";
+import Explore from "./components/Explore";
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
@@ -10,6 +14,9 @@ function App() {
       <Landing />
       <Highlights />
       <Featured />
+      <Discounted />
+      <Explore />
+      <Footer />
     </div>
   );
 }
